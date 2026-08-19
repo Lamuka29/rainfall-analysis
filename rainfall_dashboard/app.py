c0239d2bcbf15d13bec91c9bbb7c786630ca5adf
Lamuka29 <muhammadsaifulakmal29@gmail.com>
2026-08-19 09:42:22 +0800

    app.py

--- rainfall_dashboard/app.py
+++ rainfall_dashboard/app.py
@@ -378,7 +378,7 @@ BACKGROUND_COLORS = {
 # ============================================================
 
 uploaded_files = st.file_uploader(
-    "📁 Upload Excel file untuk stesen AAWS",
+    "📁 Upload Excel file data hujan mengikut stesen AAWS",
     type=["xlsx", "xls"],
     accept_multiple_files=True
 )
